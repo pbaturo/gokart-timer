50bc94a82c4865ed14deecbdaafb713d2b6ee48b
st3v1e <gdmaster113.live@gmail.com>
2025-08-23 21:03:37 +0200

    add ping endpoint to api

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{Router, routing::get};
+
+async fn ping() -> String {
+    "Pong!".to_string()
+}
+
+#[tokio::main]
+async fn main() {
+    let app = Router::new().route("/ping", get(ping));
+
+    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
+
+    axum::serve(listener, app).await.unwrap();
 }
